cd506d74c66255185e0b47afc08465829b126864
Taron Mkrtumyan <tmkrtumy@c2r4s2.42yerevan.am>
2024-10-28 14:45:26 +0400

    C03 ex00-03 check with original functions left

---
+++ ex01/ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmkrtumy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 12:47:17 by tmkrtumy          #+#    #+#             */
+/*   Updated: 2024/10/28 14:00:48 by tmkrtumy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && s1[i] == s2[i] && s1[i] && s2[i])
+		i++;
+	if (i == n || s1[i] == s2[i])
+		return (0);
+	else if (s1[i] > s2[i])
+		return (1);
+	else
+		return (-1);
+}
+
+/*int main(){
+	printf("%d\n", ft_strncmp("Thi!", "ThiA is it", 3));
+	printf("%d\n", strncmp("Thi!", "ThiA is it", 3));
+	return(0);
+}*/
---
+++ ex03/ft_strncat.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmkrtumy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 14:13:35 by tmkrtumy          #+#    #+#             */
+/*   Updated: 2024/10/28 14:28:09 by tmkrtumy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int main(){
+	char* src = " .-. ";
+	char dest[100] = "123";
+	
+	ft_strncat(dest, src, 4);
+	printf("%s\n", dest);
+	
+	strncat(dest, src, 4);
+	printf("%s\n", dest);
+	return (0);
+}*/
