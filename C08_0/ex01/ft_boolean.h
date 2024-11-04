0d7ca6c48487993c514645a3071573ec4429d801
Taron Mkrtumyan <tmkrtumy@c2r13s2.42yerevan.am>
2024-11-04 15:41:54 +0400

    C08_0 added

---
+++ C08_0/ex01/ft_boolean.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmkrtumy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 14:38:33 by tmkrtumy          #+#    #+#             */
+/*   Updated: 2024/11/04 15:28:18 by tmkrtumy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef enum t_bool
+{
+	TRUE = 1,
+	FALSE = 0,
+}	t_bool;
+
+# define EVEN(nbr) ((nbr) % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif
