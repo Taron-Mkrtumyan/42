520e984494b8346a51fe3c15c8eea481aee7e59b
Taron Mkrtumyan <tmkrtumy@c2r11s3.42yerevan.am>
2025-08-04 13:16:10 +0400

    fix: no norm errors

--- Minishell/data_init.c
+++ Minishell/data_init.c
@@ -6,7 +6,7 @@
 /*   By: tmkrtumy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 21:36:45 by tmkrtumy          #+#    #+#             */
-/*   Updated: 2025/07/30 21:50:34 by tmkrtumy         ###   ########.fr       */
+/*   Updated: 2025/08/04 13:12:25 by tmkrtumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ bool	data_init(t_data *data, int ac, char **av, char **env)
 		ft_exit(BR"Usage:\t./minishell\tOR\t./minishell -c \"command\"\n"RE);
 	if (ac == 3 && ft_strncmp("-c", av[1], 2) != 0)
 		ft_exit(BR"Interactive mode:\t./minishell -c \"command\"\n"RE);
-	if (!(data = malloc(sizeof(t_data))))
+	data = malloc(sizeof(t_data));
+	if (!data)
 		ft_exit("Data malloc failed ⛔");
 	data->env = env;
 	if (ac == 1)
