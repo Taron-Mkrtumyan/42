bd21d6e513d798cec5e73198f0b66e8327fccadc
Taron Mkrtumyan <taron.mkrtumian@gmail.com>
2025-12-31 01:36:34 +0400

    header file with correct references and structures

--- MiniRT/src/minirt.h
+++ MiniRT/src/minirt.h
@@ -19,7 +19,7 @@
 # include <stdbool.h>
 # include <X11/X.h>
 # include <X11/keysym.h>
-# include "minilibx-linux/mlx.h"
+# include "../minilibx-linux/mlx.h"
 # include <string.h>
 # include <math.h>
 # include <stdlib.h>
@@ -31,35 +31,35 @@
 # define NP_PLUS	65451
 # define NP_MINUS	65453
 
-typedef struct a_window		t_window;
-typedef struct a_vector		t_vector;
-typedef struct a_rgb		t_rgb;
-typedef struct a_obj		t_obj;
-typedef struct a_sphere		t_sphere;
-typedef struct a_cylinder	t_cylinder;
-typedef struct a_plane		t_plane;
+typedef struct s_window		t_window;
+typedef struct s_vector		t_vector;
+typedef struct s_rgb		t_rgb;
+typedef struct s_obj		t_obj;
+typedef struct s_sphere		t_sphere;
+typedef struct s_cylinder	t_cylinder;
+typedef struct s_plane		t_plane;
 
-typedef struct a_amb_light
+typedef struct s_amb_light
 {
 	float		ratio;
-	t_rgb		color;
+	t_rgb		*color;
 }	t_amb_light;
 
-typedef struct a_light
+typedef struct s_light
 {
-	t_vector	position;
+	t_vector	*position;
+	t_rgb		*color;
 	float		brightness;
-	t_rgb		color;
 }	t_light;
 
-typedef struct a_camera
+typedef struct s_camera
 {
-	t_vector	position;
-	t_vector	orientation;
+	t_vector	*position;
+	t_vector	*orientation;
 	float		fov;
 }	t_camera;
 
-typedef struct a_minirt
+typedef struct s_minirt
 {
 	t_window	*window;
 	t_camera	*camera;
@@ -89,49 +89,49 @@ typedef enum e_shape
 	PLANE,
 }	t_shape;
 
-typedef struct a_obj
+typedef struct s_obj
 {
 	t_shape	shape;
 	void	*data;	
 }	t_obj;
 
-typedef struct a_rgb
+typedef struct s_rgb
 {
 	unsigned char	r;
 	unsigned char	g;
 	unsigned char	b;
 }	t_rgb;
 
-typedef struct a_vector
+typedef struct s_vector
 {
 	float	x;
 	float	y;
 	float	z;
 }	t_vector;
 
-typedef struct a_sphere
+typedef struct s_sphere
 {
 	float		radius;
 	float		diameter;
-	t_vector	center;
-	t_rgb		color;
+	t_vector	*center;
+	t_rgb		*color;
 }	t_sphere;
 
-typedef struct a_cylinder
+typedef struct s_cylinder
 {
 	float		radius;
 	float		diameter;
 	float		height;
-	t_vector	center;
-	t_vector	normal;
-	t_rgb		color;
+	t_vector	*center;
+	t_vector	*normal;
+	t_rgb		*color;
 }	t_cylinder;
 
-typedef struct a_plane
+typedef struct s_plane
 {
-	t_vector	center;
-	t_vector	normal;
-	t_rgb		color;
+	t_vector	*center;
+	t_vector	*normal;
+	t_rgb		*color;
 }	t_plane;
 
 void	init_window(t_window *w);
