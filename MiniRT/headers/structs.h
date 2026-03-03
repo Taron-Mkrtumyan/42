fc21a16f918664a7d5974d72b7ab700995c74c94
Taron Mkrtumyan <tmkrtumy@c2r12s3.42yerevan.am>
2026-03-03 16:05:05 +0400

    right shininess

--- MiniRT/headers/structs.h
+++ MiniRT/headers/structs.h
@@ -63,7 +63,6 @@ typedef struct s_obj
 {
 	t_shape			shape;
 	void			*data;
-	double			shininess;
 	struct s_obj	*next;
 }	t_obj;
 
@@ -77,6 +76,7 @@ typedef struct s_sphere
 {
 	double		radius;
 	double		diameter;
+	double		shininess;
 	t_vec		center;
 	t_rgb		color;
 }	t_sphere;
@@ -85,6 +85,7 @@ typedef struct s_cylinder
 {
 	double		radius;
 	double		diameter;
+	double		shininess;
 	double		height;
 	t_vec		center;
 	t_vec		normal;
@@ -93,9 +94,10 @@ typedef struct s_cylinder
 
 typedef struct s_plane
 {
-	t_vec	center;
-	t_vec	normal;
-	t_rgb	color;
+	double		shininess;
+	t_vec		center;
+	t_vec		normal;
+	t_rgb		color;
 }	t_plane;
 
 typedef struct s_window		t_window;
