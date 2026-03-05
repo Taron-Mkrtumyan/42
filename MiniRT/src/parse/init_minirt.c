f9e5aaaa6e0f7a4b682e34227d362f293a6465aa
Georgiy <kankiyagg@gmail.com>
2026-03-05 19:39:35 +0400

    add get_ray

--- MiniRT/src/parse/init_minirt.c
+++ MiniRT/src/parse/init_minirt.c
@@ -28,17 +28,52 @@ void	put_pixel(t_window *win, int x, int y, t_rgb color)
 		pixel[i] = 0;
 }
 
+t_vec	get_viewport_top_left(t_minirt *rt)
+{
+	t_vec	center;
+	t_vec	horizontal;
+	t_vec	vertical;
+
+	center = vec_add(rt->camera->position, rt->viewport->normal);
+	horizontal = vec_multi(rt->viewport->right, rt->viewport->width);
+	vertical = vec_multi(rt->viewport->up, rt->viewport->height);
+	return (vec_sub(vec_add(center, vec_multi(vertical, \
+0.5)), vec_multi(horizontal, 0.5)));
+}
+
+t_ray	get_ray(t_minirt *rt, int x, int y, t_vec top_left)
+{
+	t_ray	ray;
+	t_vec	pixel;
+	double	u;
+	double	v;
+
+	u = (double)x / (rt->window->width - 1);
+	v = (double)y / (rt->window->height - 1);
+	pixel = vec_sub(vec_add(top_left, vec_multi(rt->viewport->right, \
+rt->viewport->width * u)), vec_multi(rt->viewport->up, \
+rt->viewport->height * v));
+	ray.origin = rt->camera->position;
+	ray.direction = normalize(vec_sub(pixel, ray.origin));
+	return (ray);
+}
+
 bool	init_scene(t_minirt *rt)
 {
-	int	x;
-	int	y;
+	int		x;
+	int		y;
+	t_ray	ray;
+	t_vec	top_left;
 
+	top_left = get_viewport_top_left(rt);
 	x = 0;
 	y = 0;
+	(void)ray;
 	while (y < rt->window->height)
 	{
 		while (x < rt->window->width)
 		{
+			ray = get_ray(rt, x, y, top_left);
 			put_pixel(rt->window, x, y, (t_rgb){255, 255, 255});
 			x++;
 		}
