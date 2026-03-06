1412c967bb123ef94ead721d545062d0b8383ea7
Georgii Kankiia <gkankia@c2r12s2.42yerevan.am>
2026-03-06 17:59:05 +0400

    norminette fix

--- MiniRT/src/validation/validation2.c
+++ MiniRT/src/validation/validation2.c
@@ -6,7 +6,7 @@
 /*   By: gkankia <gkankia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 18:33:56 by tmkrtumy          #+#    #+#             */
-/*   Updated: 2026/03/06 17:40:08 by gkankia          ###   ########.fr       */
+/*   Updated: 2026/03/06 17:58:52 by gkankia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,15 @@ bool	parse_camera(t_minirt *rt, char *line)
 	if (!params)
 		return (error_msg("Camera split failed"), 1);
 	if (rt->camera)
-		return (parse_error("Camera already defined", params, rt->file_line));
+		return (parse_error("Camera already defined", \
+params, rt->file_line));
 	if (ft_arrlen(params) != 4)
-		return (parse_error("Wrong number of camera parameters", params, rt->file_line));
+		return (parse_error("Wrong number of camera parameters", \
+params, rt->file_line));
 	rt->camera = ft_calloc(sizeof(t_camera), 1);
 	if (!rt->camera)
-		return (parse_error("Camera memory allocation failed", params, rt->file_line));
+		return (parse_error("Camera memory allocation failed", \
+params, rt->file_line));
 	if (!parse_camera_params(params, rt->camera))
 		return (parse_error_ptr("Invalid camera parameters",
 				NULL, params, rt->file_line));
@@ -62,13 +65,16 @@ bool	parse_resolution(t_minirt *rt, char *line)
 	if (!params)
 		return (error_msg("Resolution memory allocation failed"), 1);
 	if (ft_arrlen(params) != 3)
-		return (parse_error("Invalid resolution parameters", params, rt->file_line));
+		return (parse_error("Invalid resolution parameters", \
+params, rt->file_line));
 	while (params[++i])
 	{
 		if (i == 1 && !parse_double(params[i], &rt->window->width))
-			return (parse_error("Invalid width resolution", params, rt->file_line));
+			return (parse_error("Invalid width resolution", \
+params, rt->file_line));
 		if (i == 2 && !parse_double(params[i], &rt->window->height))
-			return (parse_error("Invalid height resolution", params, rt->file_line));
+			return (parse_error("Invalid height resolution", \
+params, rt->file_line));
 	}
 	free_arr(params);
 	return (true);
@@ -84,13 +90,15 @@ bool	parse_ambient(t_minirt *rt, char *line)
 	if (!params)
 		return (error_msg("Ambient split failed"), 1);
 	if (rt->amb_light)
-		return (parse_error("Ambient light already defined", params, rt->file_line));
+		return (parse_error("Ambient light already defined", \
+params, rt->file_line));
 	if (ft_arrlen(params) != 3)
 		return (parse_error("Wrong number of ambient light parameters", \
 params, rt->file_line));
 	rt->amb_light = ft_calloc(sizeof(t_amb_light), 1);
 	if (!rt->amb_light)
-		return (parse_error("Memory allocation failed", params, rt->file_line));
+		return (parse_error("Memory allocation failed", \
+params, rt->file_line));
 	while (params[++i])
 	{
 		if (i == 1 && !parse_double(params[i], &rt->amb_light->ratio))
@@ -115,18 +123,23 @@ bool	parse_light(t_minirt *rt, char *line)
 	if (!params)
 		return (error_msg("Light split allocation failed"), 1);
 	if (ft_arrlen(params) != 4)
-		return (parse_error("Invalid light parameters", params, rt->file_line));
+		return (parse_error("Invalid light parameters", \
+params, rt->file_line));
 	light = create_light(rt);
 	if (!light)
-		return (parse_error("Light memory allocation failed", params, rt->file_line));
+		return (parse_error("Light memory allocation failed", \
+params, rt->file_line));
 	while (params[++i])
 	{
 		if (i == 1 && !parse_vector(params[i], &light->position))
-			return (parse_error_ptr("Invalid light position", NULL, params, rt->file_line));
+			return (parse_error_ptr("Invalid light position", NULL, \
+params, rt->file_line));
 		if (i == 2 && !parse_double(params[i], &light->brightness))
-			return (parse_error_ptr("Invalid light brightness", NULL, params, rt->file_line));
+			return (parse_error_ptr("Invalid light brightness", NULL, \
+params, rt->file_line));
 		if (i == 3 && !parse_color(params[i], &light->color))
-			return (parse_error_ptr("Invalid light color", NULL, params, rt->file_line));
+			return (parse_error_ptr("Invalid light color", NULL, \
+params, rt->file_line));
 	}
 	free_arr(params);
 	return (true);
--- MiniRT/src/validation/validation3.c
+++ MiniRT/src/validation/validation3.c
@@ -6,7 +6,7 @@
 /*   By: gkankia <gkankia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 18:34:07 by tmkrtumy          #+#    #+#             */
-/*   Updated: 2026/03/06 17:43:12 by gkankia          ###   ########.fr       */
+/*   Updated: 2026/03/06 17:57:54 by gkankia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ bool	parse_shininess(char *str, double *shininess)
 	return (true);
 }
 
-bool	parse_plane(char **params, t_obj *obj, int file_line)
+bool	parse_plane(char **params, t_obj *obj, int fl)
 {
 	int		i;
 	t_plane	*plane;
@@ -29,25 +29,28 @@ bool	parse_plane(char **params, t_obj *obj, int file_line)
 	i = 0;
 	plane = ft_calloc(sizeof(t_plane), 1);
 	if (!plane)
-		return (parse_error_ptr("Memory allocation failed", plane, NULL, file_line));
+		return (parse_error_ptr("Memory allocation failed", plane, NULL, fl));
 	while (params && params[++i])
 	{
 		if (i == 1 && !parse_vector(params[i], &plane->center))
-			return (parse_error_ptr("Invalid plane center", plane, NULL, file_line));
+			return (parse_error_ptr("Invalid plane center", \
+plane, NULL, fl));
 		if (i == 2 && !parse_vector(params[i], &plane->normal))
-			return (parse_error_ptr("Invalid plane normal", plane, NULL, file_line));
+			return (parse_error_ptr("Invalid plane normal", \
+plane, NULL, fl));
 		if (i == 3 && !parse_color(params[i], &plane->color))
-			return (parse_error_ptr("Invalid plane color", plane, NULL, file_line));
+			return (parse_error_ptr("Invalid plane color", \
+plane, NULL, fl));
 		if (i == 4 && !parse_shininess(params[i], &plane->shininess))
 			return (parse_error_ptr("Invalid plane shininess value", \
-plane, NULL,file_line));
+plane, NULL, fl));
 	}
 	plane->normal = normalize(plane->normal);
 	obj->data = plane;
 	return (true);
 }
 
-bool	parse_sphere(char **params, t_obj *obj, int file_line)
+bool	parse_sphere(char **params, t_obj *obj, int fl)
 {
 	int			i;
 	t_sphere	*sphere;
@@ -56,18 +59,21 @@ bool	parse_sphere(char **params, t_obj *obj, int file_line)
 	sphere = ft_calloc(sizeof(t_sphere), 1);
 	if (!sphere)
 		return (parse_error_ptr("Sphere memory allocation failed", \
-sphere, NULL, file_line));
+sphere, NULL, fl));
 	while (params && params[++i])
 	{
 		if (i == 1 && !parse_vector(params[i], &sphere->center))
-			return (parse_error_ptr("Invalid sphere center", sphere, NULL, file_line));
+			return (parse_error_ptr("Invalid sphere center", \
+sphere, NULL, fl));
 		if (i == 2 && !parse_double(params[i], &sphere->diameter))
-			return (parse_error_ptr("Invalid sphere diameter", sphere, NULL, file_line));
+			return (parse_error_ptr("Invalid sphere diameter", \
+sphere, NULL, fl));
 		if (i == 3 && !parse_color(params[i], &sphere->color))
-			return (parse_error_ptr("Invalid sphere color", sphere, NULL, file_line));
+			return (parse_error_ptr("Invalid sphere color", \
+sphere, NULL, fl));
 		if (i == 4 && !parse_shininess(params[i], &sphere->shininess))
 			return (parse_error_ptr("Invalid sphere shininess value", \
-sphere, NULL, file_line));
+sphere, NULL, fl));
 	}
 	sphere->radius = sphere->diameter / 2.0;
 	obj->data = sphere;
@@ -80,7 +86,7 @@ static void	init_cylinder(t_cylinder *cylinder)
 	cylinder->radius = cylinder->diameter / 2.0;
 }
 
-bool	parse_cylinder(char **params, t_obj *obj, int file_line)
+bool	parse_cylinder(char **params, t_obj *obj, int fl)
 {
 	int			i;
 	t_cylinder	*cylinder;
@@ -88,21 +94,28 @@ bool	parse_cylinder(char **params, t_obj *obj, int file_line)
 	i = 0;
 	cylinder = ft_calloc(sizeof(t_cylinder), 1);
 	if (!cylinder)
-		return (parse_error_ptr("Memory allocation failed", cylinder, NULL, file_line));
+		return (parse_error_ptr("Memory allocation failed", \
+cylinder, NULL, fl));
 	while (params && params[++i])
 	{
 		if (i == 1 && !parse_vector(params[i], &cylinder->center))
-			return (parse_error_ptr("Invalid cylinder center", cylinder, NULL, file_line));
+			return (parse_error_ptr("Invalid cylinder center", \
+cylinder, NULL, fl));
 		if (i == 2 && !parse_vector(params[i], &cylinder->normal))
-			return (parse_error_ptr("Invalid cylinder normal", cylinder, NULL, file_line));
+			return (parse_error_ptr("Invalid cylinder normal", \
+cylinder, NULL, fl));
 		if (i == 3 && !parse_double(params[i], &cylinder->diameter))
-			return (parse_error_ptr("Invalid cylinder diam", cylinder, NULL, file_line));
+			return (parse_error_ptr("Invalid cylinder diam", \
+cylinder, NULL, fl));
 		if (i == 4 && !parse_double(params[i], &cylinder->height))
-			return (parse_error_ptr("Invalid cylinder height", cylinder, NULL, file_line));
+			return (parse_error_ptr("Invalid cylinder height", \
+cylinder, NULL, fl));
 		if (i == 5 && !parse_color(params[i], &cylinder->color))
-			return (parse_error_ptr("Invalid cylinder color", cylinder, NULL, file_line));
+			return (parse_error_ptr("Invalid cylinder color", \
+cylinder, NULL, fl));
 		if (i == 6 && !parse_shininess(params[i], &cylinder->shininess))
-			return (parse_error_ptr("Cylinder shininess !!", cylinder, NULL, file_line));
+			return (parse_error_ptr("Cylinder shininess !!", \
+cylinder, NULL, fl));
 	}
 	init_cylinder(cylinder);
 	obj->data = cylinder;
